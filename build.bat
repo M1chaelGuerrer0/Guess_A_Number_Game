34b8ecf17c116252a0d98eb2ce3467a9bba0dd39
M1chaelGuerrer0 <michael.guerrero.013@my.csun.edu>
2024-02-01 23:09:08 -0800

    updating

--- build.bat
+++ build.bat
@@ -5,8 +5,7 @@ IF NOT EXIST bin-int mkdir bin-int
 
 pushd bin-int
 
-cl ..\main.cpp ..\game.cpp /c
-cl main.obj game.obj /link /out:run.exe
+cl ..\main.cpp /link /out:run.exe
 
 popd
 
--- main.cpp
+++ main.cpp
@@ -2,7 +2,46 @@
 #include <stdlib.h>
 #include <time.h>
 
-int main() {
-    
+// Author: Michael Guerrero
+ // Guess_A_Number_Game
+ //  Generates random integer from 0 ... 100
+ //  Each attempt at the answer will provide a higher or lower prompt   
+ //  Compiles and executes at the command prompt
+
+int main() { // main
+    printf("\nWelcome to Guess_A_Number_Game!\nI will pick a random number from 0 to 99 and you will guess the number.\n");
+    srand(time(NULL)); // random seed
+    int randNumber = rand() % 101; // 0-100
+    int var;
+    char end;
+    printf("\nEnter a number: ");
+    scanf("%d", &var);
+    // while loop
+    while(var != randNumber) {
+        printf("\nYou guessed wrong :(\n");
+        if(var > randNumber) {
+            printf("It is lower than %d\n", var);
+        }
+        else {
+            printf("It is higher than %d\n", var);
+        }
+        printf("Try again? (y/n)\n");
+        while(true) { // 2nd while loop
+        scanf("%c", &end);
+        if(end == 'n') {
+            return 0;
+        }
+        else if (end == 'y') {
+            printf("\nNever give up!");
+            printf("\nEnter a number: ");
+            scanf("%d", &var);
+            break;
+        }
+        else {
+            continue;
+            }
+        } // 2nd while loop ends
+    } // end while loop
+    printf("\nYou guessed right!");
     return 0;
-}
+} // end main
